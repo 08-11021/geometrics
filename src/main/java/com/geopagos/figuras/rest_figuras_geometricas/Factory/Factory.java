0663b2bdb21257d69cd328b0ab90c91a16f442c7
08-11021 <globerusso@gmail.com>
2018-05-28 16:36:30 -0300

    factory improvement

--- src/main/java/com/geopagos/figuras/rest_figuras_geometricas/Factory/Factory.java
+++ src/main/java/com/geopagos/figuras/rest_figuras_geometricas/Factory/Factory.java
@@ -1,19 +1,23 @@
 package com.geopagos.figuras.rest_figuras_geometricas.Factory;
 
+import com.geopagos.figuras.rest_figuras_geometricas.BOConf;
+import com.geopagos.figuras.rest_figuras_geometricas.geometrics.BO.Circle;
 import com.geopagos.figuras.rest_figuras_geometricas.geometrics.BO.Figure;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Factory {
-    private static BeanFactory factory = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
-    private Figure figure;
+    private ApplicationContext factory = new AnnotationConfigApplicationContext(BOConf.class);
 
-    public Factory(String type){
-        this.figure = (Figure)factory.getBean(type);
+    public Factory(){
     }
 
-    public Figure getFigure(){
-        return figure;
+    public Figure getFigure(String type){
+        return (Figure)factory.getBean(type);
+
     }
 
 }
